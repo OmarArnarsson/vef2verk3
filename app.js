016391153c07294aa98fbc7653b12a1d9c10bde0
dezimba <omararnars@gmail.com>
2019-02-23 22:15:29 +0000

    koma svo heroku

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const app = express();
 /* todo stilla session og passport */
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
+  name: 'counter.sid',
   secret: sessionSecret,
   resave: false,
   saveUninitialized: false,
@@ -156,8 +157,10 @@ function errorHandler(error, req, res, next) { // eslint-disable-line
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
+const {
+  HOST: hostname = '127.0.0.1',
+  PORT: port = 3000,
+} = process.env;
 
 
 app.listen(port, () => {
